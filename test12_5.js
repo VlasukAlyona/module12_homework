5e569e2db4c71fade3bc466f51d5be9d3fab757d
VlasukAlyona <tan.vl@mail.ru>
2020-08-14 18:11:39 +0300

    Update test12_5.js

--- test12_5.js
+++ test12_5.js
@@ -5,15 +5,31 @@ class ElectroDevice {
  this.mod = mod;
  }
  GetPower(){
-  console.log(`${name} потребляет ${pow} Ватт энергии`);
+  console.log(`${this.name} потребляет ${this.pow} Ватт энергии`);
 }
  GetMode(){
-  if (mod == 1) {
-      console.log(`${name} включён в розетку`);
+  if (this.mod == 1) {
+      console.log(`${this.name} включён в розетку`);
  } else {
-      console.log(`${name} выключен из розетки`);
+      console.log(`${this.name} выключен из розетки`);
  }
 }
 }
 const fen = new ElectroDevice('Фен', 1500, 1)
+fen.city = 'Китай';
+fen.Age = function(age){
+  console.log(`Год производительности фена ${age}лет, город ${fen.city}`);
+}
+
+fen.Age(5)
 fen.GetPower()
+fen.GetMode()
+
+const computer = new ElectroDevice('Компьютер', 220, 0);
+computer.brand = 'MSI';
+computer.Color = function(color){
+  console.log(`Компьютер марки ${computer.brand} ${color} цвета`);
+}
+computer.GetPower()
+computer.GetMode()
+computer.Color('белого')
